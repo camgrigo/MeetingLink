8152fb110b300432099e0f5cf71593f03b7c195f
Cameron Grigoriadis <camgrigoriadis@gmail.com>
2022-05-01 19:32:35 -0700

    Finish settings screen

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,82 +9,87 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    
+    var url: URL? {
+        URL(string: "zoomus://")
+    }
+    
+    @State private var isShowingSettings = false
+    @State private var openLinkOnOpen = false
+    
+    @ObservedObject var meeting: Meeting
+    
+    enum Field {
+        case url // id, passcode
+    }
+    @FocusState var field: Field?
+    
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
+        CallButton(action: openInZoom)
+            .disabled(url == nil || meeting.urlString == nil || !UIApplication.shared.canOpenURL(url!))
+            .padding()
             .toolbar {
-#if os(iOS)
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-#endif
                 ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
+                    Button {
+                        isShowingSettings = true
+                    } label: {
+                        Label("Settings", systemImage: "gearshape")
+                    }
+                    .sheet(isPresented: $isShowingSettings) {
+                        NavigationView {
+                            List {
+                                Section {
+                                    VStack(alignment: .leading) {
+                                            Label("Link", systemImage: "link")
+                                            .font(.headline)
+                                            HStack {
+                                                TextEditor(text: Binding { meeting.urlString ?? "" } set: {
+                                                    meeting.urlString = $0
+                                                    try! PersistenceController.shared.container.viewContext.save()
+                                                })
+                                                .onChange(of: meeting.urlString, perform: { newValue in
+                                                    try! PersistenceController.shared.container.viewContext.save()
+                                                })
+                                                .focused($field, equals: .url)
+                                                .padding(.trailing)
+                                                Button {
+                                                    meeting.urlString = ""
+                                                } label: {
+                                                    Image(systemName: "xmark")
+                                                }
+                                            }
+                                    }
+                                }
+//                                Section {
+//                                    Toggle(isOn: $openLinkOnOpen) {
+//                                        Text("Join Automatically")
+//                                    }
+//                                }
+                            }
+                            .navigationTitle("Settings")
+                            .toolbar {
+                                Button {
+                                    isShowingSettings = false
+                                } label: {
+                                    Text("Done").bold()
+                                }
+
+                            }
+                        }
+                    }
+                }
+                ToolbarItem(placement: .keyboard) {
+                    Button("Done") {
+                        field = nil
                     }
                 }
             }
-            Text("Select an item")
+    }
+    
+    func openInZoom() {
+        if let url = url, let urlString = meeting.urlString, let zoomLink = URL(string: urlString), UIApplication.shared.canOpenURL(url) {
+            UIApplication.shared.open(zoomLink)
         }
     }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-}
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
-    }
+    
 }
--- Shared/MeetingsApp.swift
+++ Shared/MeetingsApp.swift
@@ -9,12 +9,41 @@ import SwiftUI
 
 @main
 struct MeetingsApp: App {
+    
     let persistenceController = PersistenceController.shared
-
+    
+    @State private var meeting: Meeting?
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            if let meeting = meeting {
+                NavigationView {
+                    ContentView(meeting: meeting)
+                        .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                }
+                .navigationViewStyle(.stack)
+            } else {
+                VStack {
+                    ProgressView("Loading…")
+                }.onAppear {
+                    let settingsFetch = Settings(context: persistenceController.container.viewContext)
+                    
+                    let fetch = Meeting.fetchRequest()
+                    
+                    let meetings = try! PersistenceController.shared.container.viewContext.fetch(fetch)
+                    
+                    if let first = meetings.first {
+                        meeting = first
+                        
+                    } else {
+                        let m = Meeting(context: persistenceController.container.viewContext)
+                        m.id = UUID().uuidString
+                        try! persistenceController.container.viewContext.save()
+                        meeting = m
+                    }
+                }
+            }
         }
     }
+    
 }
